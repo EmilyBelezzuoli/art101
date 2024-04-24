71e3707f0c15aee2bf6091712b85a05612acf750
EmilyBelezzuoli <166079144+EmilyBelezzuoli@users.noreply.github.com>
2024-04-23 18:27:18 -0700

    lab 4

---
+++ lab4/js/lab6.js
@@ -0,0 +1,30 @@
+// Lab 4: Stippling
+// The basic beginning skills
+// Author: Emily Belezzuoli and Celeste Hernandez
+// Date: April 23, 2024
+
+// Pick paper
+    // Pick pens/width
+        // Obtain/Sketch outline
+        // Focus on tones and lighting
+        // Dot pattern/condensed or uncondensed
+    // Frequency of dots
+        // Look at work from certain angle
+    // If needed, add more depth into work
+// Admire and then Finish
+
+// Lab 4: Hangman
+// A guessing game
+// Author: Emily Belezzuoli and Celeste Hernandez
+// Date: April 23, 2024
+
+// Set up a word for someone to guess
+// Set up podium ( a 7)
+// Guess letters
+// If true add letters to blanks
+    // For each letter wrong, draw body part of man
+        // For the first incorrect guess: head
+        // For the second incorrect guess: body
+        // For the third + fourth incorrect guess: arms
+        // For the fifth + six incorrect guess: legs
+    // If you guess incorrectly six times before guessing the selected word: YOU LOSE!
