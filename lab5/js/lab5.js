0022ac1fa98e48c0bc24c50c05fdb55e544ddc6d
EmilyBelezzuoli <166079144+EmilyBelezzuoli@users.noreply.github.com>
2024-04-26 02:42:29 -0700

    lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+// Lab 5: Data Types and Variables
+// Declaring and Outputting Variables
+// Author: Emily Belezzuoli
+// Date: April 26, 2024
+
+// Declare Variables
+let carMake = "MiniCooper";
+let carModel = "Hardtop 2 Door";
+let CarYear = 2022;
+let carColor = "Grey and blue";
+let ownIt = true;
+let currentYear = 2024;
+let carAge = currentYear-carYear;
+
+
+// Output Variables
+document.writeln("Car make: ",CarMake,"<br>");
+document.writeln("Car model: ",CarModel,"<br>");
+document.writeln("Car year: ",CarYear,"<br>");
+document.writeln("Car color: ",CarColor,"<br>");
+document.writeln("Car age: ",carAge,"<br>");
