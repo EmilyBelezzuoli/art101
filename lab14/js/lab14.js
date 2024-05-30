6a8726b4124d1c9651231e7b4b1426add3ede9b4
EmilyBelezzuoli <166079144+EmilyBelezzuoli@users.noreply.github.com>
2024-05-29 22:21:40 -0700

    lab 14

---
+++ lab14/js/lab14.js
@@ -0,0 +1,9 @@
+// Lab 14 - Debugging tools and strategies
+// lab.js - create a function that depends on conditionals
+// Requirements: jQuery must be loaded for this script to work
+// Author: Emily Belezzuoli
+// Date: May 28, 2024
+
+  
+  
+  
